587ac22546b07ebcbda55a032e2ae4dcdb9b0348
Anthony Rojas <a20145976@pucp.pe>
2020-06-01 17:54:53 -0500

    8

--- src/main/java/sw2/lab5/Controllers/PostController.java
+++ src/main/java/sw2/lab5/Controllers/PostController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sw2.lab5.Entity.Post;
 import sw2.lab5.Repository.PostRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value ="/posts")
@@ -20,9 +24,17 @@ public class PostController {
         return "index";
     }
 
+    @GetMapping("borrar")
     public String borrar(@RequestParam("id") int id, Model model){
-        model.addAttribute("lista", postRepository.findAll());
-        return "index";
+        Optional<Post> opt = postRepository.findById(id);
+        if (opt.isPresent()){
+            postRepository.deleteById(id);
+        }
+        return "redirect:/posts";
     }
 
+    @GetMapping("nuevo")
+    public String nuevo(@ModelAttribute("post") Post post){
+        return "post/Form";
+    }
 }
